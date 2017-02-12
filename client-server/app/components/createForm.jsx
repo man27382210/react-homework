ac9030e7388c01f36ae7de29f16d08859e4c6269
Chun-MingChen <nick830314@gmail.com>
2017-02-12 21:25:58 +0800

    redux for createForm

--- client-server/app/components/createForm.jsx
+++ client-server/app/components/createForm.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { onCreateFormSubmit } from '../actions/';
+import Constant from '../common/constant';
 
-export default class App extends React.Component {
+class CreateForm extends React.Component {
   constructor(props) {
     super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.resetForm = this.resetForm.bind(this);
+    this.onFormChange = this.onFormChange.bind(this);
+    this.state = {
+      owner: Constant.OWNER_DEFAULT,
+      title: Constant.TITLE_DEFAULT,
+      category: Constant.CATEGORY_DEFAULT,
+      status: Constant.STATUS_DEFAULT,
+      priority: Constant.PRIORITY_DEFAULT,
+    };
+  }
+  resetForm() {
+    this.setState({
+      owner: Constant.OWNER_DEFAULT,
+      title: Constant.TITLE_DEFAULT,
+      category: Constant.CATEGORY_DEFAULT,
+      status: Constant.STATUS_DEFAULT,
+      priority: Constant.PRIORITY_DEFAULT,
+    });
   }
   onSubmit(event) {
+    let valueColumnName;
+
     // prevent default form action
     event.preventDefault();
+
+    // get value from input form
     const opt = {
       owner: document.getElementById('create-form-owner').value,
       title: document.getElementById('create-form-title').value,
@@ -15,9 +41,30 @@ export default class App extends React.Component {
       priority: document.getElementById('create-form-priority').value,
     };
 
-    console.log(`submit and\n ${JSON.stringify(opt)}`);
-  }
+    // check existance of each column
+    const emptyValueExist = Object.keys(opt).every((key) => {
+      const result = opt[key] ? true : false;
+      if (!result) valueColumnName = key;
+      return result;
+    });
 
+    // send action to create new form element
+    if (emptyValueExist) {
+      this.props.onCreateFormSubmit(opt);
+      this.resetForm();
+    } else {
+      console.log(`column '${valueColumnName}' is empty`);
+    }
+  }
+  onFormChange(event) {
+    if (this.state.hasOwnProperty(event.target.name)) {
+      const value = {};
+      value[event.target.name] = event.target.value;
+      this.setState({
+        ...value
+      });
+    }
+  }
   render() {
     const createFormStyle = {
       padding: '20px',
@@ -29,41 +76,72 @@ export default class App extends React.Component {
         <form className="col s12">
           <div className="row">
             <div className="input-field col s6">
-              <input id="create-form-owner" type="text" className="validate" />
+              <input
+                id="create-form-owner"
+                type="text"
+                className="validate"
+                name="owner"
+                onChange={this.onFormChange}
+                value={this.state.owner}
+              />
               <label>Owner name</label>
             </div>
           </div>
 
           <div className="row">
             <div className="input-field col s6">
-              <input id="create-form-title" type="text" className="validate" />
+              <input
+                id="create-form-title"
+                type="text"
+                className="validate"
+                onChange={this.onFormChange}
+                value={this.state.title}
+                name="title"
+              />
               <label >Title</label>
             </div>
             <div className="input-field col s6">
-              <input id="create-form-category" type="text" className="validate" />
+              <input
+                id="create-form-category"
+                type="text"
+                className="validate"
+                onChange={this.onFormChange}
+                value={this.state.category}
+                name="category"
+              />
               <label >category </label>
             </div>
           </div>
 
           <div className="row">
             <div className="input-field col s3">
-              <select id="create-form-status" value={0}>
-                <option value="0" disabled>Choose your status</option>
-                <option value="1">open</option>
-                <option value="2">pending</option>
-                <option value="3">processing</option>
-                <option value="4">closed</option>
+              <select
+                id="create-form-status"
+                onChange={this.onFormChange}
+                value={this.state.status}
+                name="status"
+              >
+                <option value={Constant.STATUS_DEFAULT} disabled>Choose your status</option>
+                <option value={Constant.STATUS_OPEN}>open</option>
+                <option value={Constant.STATUS_PENDING}>pending</option>
+                <option value={Constant.STATUS_PROCESSING}>processing</option>
+                <option value={Constant.STATUS_CLOSED}>closed</option>
               </select>
               <label>Status </label>
             </div>
 
             <div className="input-field col s3">
-              <select id="create-form-priority"value={0}>
-                <option value="0" disabled>Choose your priority</option>
-                <option value="1">Emergency</option>
-                <option value="2">important</option>
-                <option value="3">normal</option>
-                <option value="4">nothing</option>
+              <select
+                id="create-form-priority"
+                onChange={this.onFormChange}
+                value={this.state.priority}
+                name="priority"
+              >
+                <option value={Constant.PRIORITY_DEFAULT} disabled>Choose your priority</option>
+                <option value={Constant.PRIORITY_EMERGENCY}>Emergency</option>
+                <option value={Constant.PRIORITY_IMPORTANT}>important</option>
+                <option value={Constant.PRIORITY_NORAML}>normal</option>
+                <option value={Constant.PRIORITY_NOTHING}>nothing</option>
               </select>
               <label>Priority </label>
             </div>
@@ -80,3 +158,8 @@ export default class App extends React.Component {
     );
   }
 }
+CreateForm.PropTypes = {
+  onCreateFormSubmit: PropTypes.func
+};
+
+export default connect(null, { onCreateFormSubmit })(CreateForm);
