b34d7200a5991b66d3acaeee70b9c22317d97f2e
Chun-MingChen <nick830314@gmail.com>
2017-02-12 21:26:12 +0800

    redux for editModal

--- client-server/app/components/editModal.jsx
+++ client-server/app/components/editModal.jsx
@@ -1,69 +1,139 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { onModalEdit, onElementItemEdit } from  '../actions/';
+import Constant from '../common/constant';
 
-export default () => {
-  // Modal Structure
-  const style = {
-    marginTop: '16px',
-    marginBottom: '16px',
-    paddingRight: '32px'
-  };
-  return (
-    <div>
-      <div id="edit-modal" className="modal">
-        <div className="modal-content">
-          <div className="row">
-            <div className="input-field col s6">
-              <input id="owner" type="text" className="validate" />
-              <label>Owner name</label>
+class EditModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onFormChange = this.onFormChange.bind(this);
+    this.state = {
+      ...this.props.modalDisplay
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    // receive central state and set local state
+    this.setState({
+      ...nextProps.modalDisplay
+    });
+  }
+  onFormChange(event) {
+    if (this.state.hasOwnProperty(event.target.name)) {
+      const value = {};
+      value[event.target.name] = event.target.value;
+      this.setState({
+        ...value
+      });
+    }
+  }
+  render() {
+    const style = {
+      marginTop: '16px',
+      marginBottom: '16px',
+      paddingRight: '32px'
+    };
+    return (
+      <div>
+        <div id="edit-modal" className="modal">
+          <div className="modal-content">
+            <div className="row">
+              <div className="input-field col s6">
+                <input
+                  id="edit-modal-owner"
+                  type="text"
+                  className="validate"
+                  name="owner"
+                  onChange={this.onFormChange}
+                  value={this.state.owner}
+                />
+                <label>Owner name</label>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s6">
+                <input
+                  id="edit-modal-title"
+                  type="text"
+                  className="validate"
+                  onChange={this.onFormChange}
+                  value={this.state.title}
+                  name="title"
+                />
+                <label >Title</label>
+              </div>
+              <div className="input-field col s6">
+                <input
+                  id="edit-modal-category"
+                  type="text"
+                  className="validate"
+                  onChange={this.onFormChange}
+                  value={this.state.category}
+                  name="category"
+                />
+                <label >category </label>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s3">
+                <select
+                  id="edit-modal-status"
+                  onChange={this.onFormChange}
+                  value={this.state.status}
+                  name="status"
+                >
+                  <option value={Constant.STATUS_DEFAULT} disabled>Choose your status</option>
+                  <option value={Constant.STATUS_OPEN}>open</option>
+                  <option value={Constant.STATUS_PENDING}>pending</option>
+                  <option value={Constant.STATUS_PROCESSING}>processing</option>
+                  <option value={Constant.STATUS_CLOSED}>closed</option>
+                </select>
+                <label>Status </label>
+              </div>
+
+              <div className="input-field col s3">
+                <select
+                  id="edit-modal-priorty"
+                  onChange={this.onFormChange}
+                  value={this.state.priority}
+                  name="priority"
+                >
+                  <option value={Constant.PRIORITY_DEFAULT} disabled>Choose your priority</option>
+                  <option value={Constant.PRIORITY_EMERGENCY}>Emergency</option>
+                  <option value={Constant.PRIORITY_IMPORTANT}>important</option>
+                  <option value={Constant.PRIORITY_NORAML}>normal</option>
+                  <option value={Constant.PRIORITY_NOTHING}>nothing</option>
+                </select>
+                <label>Priority </label>
+              </div>
+
             </div>
           </div>
-
-          <div className="row">
-            <div className="input-field col s6">
-              <input id="title" type="text" className="validate" />
-              <label >Title</label>
-            </div>
-            <div className="input-field col s6">
-              <input id="category" type="text" className="validate" />
-              <label >category </label>
-            </div>
+          <div className="modal-footer" style={style}>
+            <button
+              className="modal-action modal-close btn waves-effect waves-light indigo"
+              onClick={this.props.onModalEdit}
+            >
+              Edit
+              <i className="material-icons right">mode_edit</i>
+            </button>
           </div>
-
-          <div className="row">
-            <div className="input-field col s3">
-              <select value={0}>
-                <option value="0" disabled>Choose your status</option>
-                <option value="1">open</option>
-                <option value="2">pending</option>
-                <option value="3">processing</option>
-                <option value="4">closed</option>
-              </select>
-              <label>Status </label>
-            </div>
-
-            <div className="input-field col s3">
-              <select value={0}>
-                <option value="0" disabled>Choose your priority</option>
-                <option value="1">Emergency</option>
-                <option value="2">important</option>
-                <option value="3">normal</option>
-                <option value="4">nothing</option>
-              </select>
-              <label>Priority </label>
-            </div>
-
-          </div>
-
-
-
-        </div>
-        <div className="modal-footer" style={style}>
-          <button className="modal-action modal-close btn waves-effect waves-light indigo">
-            Edit
-            <i className="material-icons right">mode_edit</i>
-          </button>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+EditModal.propTypes = {
+  modalDisplay: PropTypes.object,
+  onModalEdit: PropTypes.func,
+  onElementItemEdit: PropTypes.func,
 };
+
+function mapStateToProps(state) {
+  return {
+    modalDisplay: state.modalDisplay
+  };
+}
+
+export default connect(mapStateToProps, { onModalEdit, onElementItemEdit })(EditModal);
