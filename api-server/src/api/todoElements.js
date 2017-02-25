b81286bb4a43b31afbf361b3cd78b3542f6565ac
Chun-MingChen <nick830314@gmail.com>
2017-02-25 19:17:12 +0800

    rename facet to todoElement

--- api-server/src/api/todoElements.js
+++ api-server/src/api/todoElements.js
@@ -1,50 +1,50 @@
 import resource from 'resource-router-middleware';
-import facets from '../models/facets';
+import todoElements from '../models/todoElements';
 
 export default ({ config, db }) => resource({
 
 	/** Property name to store preloaded entity on `request`. */
-	id : 'facet',
+	id : 'todoElement',
 
 	/** For requests with an `id`, you can auto-load the entity.
 	 *  Errors terminate the request, success sets `req[id] = data`.
 	 */
 	load(req, id, callback) {
-		let facet = facets.find( facet => facet.id===id ),
-			err = facet ? null : 'Not found';
-		callback(err, facet);
+		let todoElement = todoElements.find( todoElement => todoElement.id===id ),
+			err = todoElement ? null : 'Not found';
+		callback(err, todoElement);
 	},
 
 	/** GET / - List all entities */
 	index({ params }, res) {
-		res.json(facets);
+		res.json(todoElements);
 	},
 
 	/** POST / - Create a new entity */
 	create({ body }, res) {
-		body.id = facets.length.toString(36);
-		facets.push(body);
+		body.id = todoElements.length.toString(36);
+		todoElements.push(body);
 		res.json(body);
 	},
 
 	/** GET /:id - Return a given entity */
-	read({ facet }, res) {
-		res.json(facet);
+	read({ todoElement }, res) {
+		res.json(todoElement);
 	},
 
 	/** PUT /:id - Update a given entity */
-	update({ facet, body }, res) {
+	update({ todoElement, body }, res) {
 		for (let key in body) {
 			if (key!=='id') {
-				facet[key] = body[key];
+				todoElement[key] = body[key];
 			}
 		}
 		res.sendStatus(204);
 	},
 
 	/** DELETE /:id - Delete a given entity */
-	delete({ facet }, res) {
-		facets.splice(facets.indexOf(facet), 1);
+	delete({ todoElement }, res) {
+		todoElements.splice(todoElements.indexOf(todoElement), 1);
 		res.sendStatus(204);
 	}
 });
---
+++ api-server/src/models/todoElements.js
@@ -0,0 +1,4 @@
+import fakeData from './fakeData.json';
+// our example model is just an Array
+const todoElements = fakeData;
+export default todoElements;
