076642390210d79976914b261973923d48ceb23f
Chun-MingChen <nick830314@gmail.com>
2017-03-18 20:37:02 +0800

    init for react-router on server

--- client-server/server-side-rendering.js
+++ client-server/server-side-rendering.js
@@ -3,7 +3,8 @@ import { renderToStaticMarkup } from 'react-dom/server';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './app/reducers/';
-import PageIndex from './app/components/page_index/index.jsx';
+import { match, RouterContext } from 'react-router';
+import routes from './app/routes/';
 
 function renderFullPage(html) {
   return (`
@@ -18,17 +19,17 @@ function renderFullPage(html) {
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link rel="stylesheet" href="css/materialize.min.css">
       <link rel="stylesheet" href="css/csspin-balls.css">
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     </head>
     <body>
       <div id="root">${html}</div>
-      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script src="js/materialize.min.js"></script>
       <script >
         $(document).ready(function() {
           $('select').material_select();
           $('.modal').modal();
         });
       </script>
+      <script src="js/materialize.min.js"></script>
       <script src="main.js"></script>
     </body>
   </html>
@@ -39,11 +40,24 @@ function renderFullPage(html) {
 module.exports = (req, res) => {
   // Create a new Redux store instance
   const store = createStore(reducers);
-  const html = renderToStaticMarkup(
-    <Provider store={store}>
-      <PageIndex />
-    </Provider>
-  );
-  const page = renderFullPage(html);
-  res.send(page);
+
+  // SSR for react-router
+  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+    if (error) {
+      res.status(500).send(error.message);
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+    } else if (renderProps) {
+      const html = renderToStaticMarkup(
+        <Provider store={store}>
+          <RouterContext {...renderProps} />
+        </Provider>
+      );
+      const page = renderFullPage(html);
+      res.status(200).send(page);
+    } else {
+      res.status(404).send('Not found');
+    }
+  });
 };
+
